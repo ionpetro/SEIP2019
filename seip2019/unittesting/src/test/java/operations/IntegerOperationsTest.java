58accff0c7348a62d3ec5da806e46b3134de1e69
ionpetro <ionpetro@gmail.com>
2019-04-11 10:53:36 +0300

    Additional test cases for PowerOfTwo method

--- seip2019/unittesting/src/test/java/operations/IntegerOperationsTest.java
+++ seip2019/unittesting/src/test/java/operations/IntegerOperationsTest.java
@@ -1,7 +1,11 @@
 package operations;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.Assert;
+import org.junit.Rule;
+
+//TODO change method names
 
 public class IntegerOperationsTest {
 	IntegerOperations io = new IntegerOperations();
@@ -31,8 +35,42 @@ public class IntegerOperationsTest {
 	@Test (expected = IllegalArgumentException.class)
 	public void test_add_overflow() {
 		int x = Integer.MAX_VALUE;
-		int y = Integer.MAX_VALUE;
+		int y = 1;
 		io.add(x, y);
 	}
+	
+	@Test
+	public void test_powerOfTwo_normalCase() {
+		Assert.assertEquals(16, io.powerOfTwo(4));
+	}
+	
+	@Test
+	public void test_powerOfTwo_ZeroInteger() {
+		Assert.assertEquals(1, io.powerOfTwo(0));	
+	}
+	
+	@Test
+	public void test_powerOfTwo_OneCase() {
+		Assert.assertEquals(2, io.powerOfTwo(1));
+		
+	}
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
+	@Test
+	public void test_powerOfTwo_negative() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage(" is not a positive integer.");
+		io.powerOfTwo(-3);
+	}
+	
+	@Test
+	public void test_powerOfTwo_overflow() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("^2 causes an integer overflow.");
+		io.powerOfTwo(31);
+	}
+	
 	
 }
