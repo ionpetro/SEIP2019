75475e578106d114056abb852afff606dc0287f0
ionpetro <ionpetro@gmail.com>
2019-04-12 01:17:18 +0300

    Add tests for ArrayOperations java file

--- seip2019/unittesting/src/test/java/operations/ArrayOperationsTest.java
+++ seip2019/unittesting/src/test/java/operations/ArrayOperationsTest.java
@@ -3,6 +3,48 @@ package operations;
 import org.junit.Test;
 import org.junit.Assert;
 
+import filehandlers.FileIO;
+import static org.mockito.Mockito.*;
+
 public class ArrayOperationsTest {
+
+	ArrayOperations arop;
+
+	//TODO failed test
+	@Test
+	public void test_reverseArray_Mocking() {
+
+		FileIO fi = mock(FileIO.class);
+		IntegerOperations io = mock(IntegerOperations.class);
+
+		String filepath = "src/test/resources/FileIo_ValidTestFile.txt";
+
+		when(io.reverseSign(1)).thenReturn(-1);
+		when(io.reverseSign(2)).thenReturn(-2);
+		when(io.reverseSign(-3)).thenReturn(3);
+		when(io.reverseSign(-4)).thenReturn(4);
+
+		int[] fileInput = { 1, 2, -3, -4 };
+		when(fi.readFile(filepath)).thenReturn(fileInput);
+
+		int[] testValues = { -1, -2, 3, 4 };
+		Assert.assertArrayEquals(testValues, arop.reverseArray(filepath));
+
+	}
+
+	//TODO failed test
+	@Test
+	public void test_findMaxInFile_Mocking() {
+
+		FileIO fi = mock(FileIO.class);
+
+		String filepath = "src/test/resources/ArrayTest.txt";
+
+		int[] fileInput = { 1, 2, 3, 4 };
+		when(fi.readFile(filepath)).thenReturn(fileInput);
+
+		Assert.assertEquals(4, arop.findMaxInFile(filepath));
+
+	}
 
 }
