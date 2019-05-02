052ce1171b50f3c9af4ea08c774354868a106d55
ionpetro <ionpetro@gmail.com>
2019-05-02 19:33:35 +0300

    Fix issue #13

--- seip2019/unittesting/src/test/java/operations/ArrayOperationsTest.java
+++ seip2019/unittesting/src/test/java/operations/ArrayOperationsTest.java
@@ -7,26 +7,25 @@ import filehandlers.FileIO;
 import static org.mockito.Mockito.*;
 
 /**
- * An class that provides test cases for the 
- * reverseArray method of ArrayOperations
- * class.
+ * An class that provides test cases for the reverseArray method of
+ * ArrayOperations class.
+ * 
  * @author ionpetro (ionpetro@gmail.com)
  */
-public class ArrayOperationsTest {	
+public class ArrayOperationsTest {
 
 	/*
-	 * The following test aims to fix the problem described in 
-	 * the previous test_reverseArray test case. In order to 
-	 * isolate the external (to this class) dependencies (FileIO)
-	 * we have to mock them and predefine the results of
-	 * all calls that we plan to execute on the FileIO methods.
+	 * The following test aims to fix the problem described in the previous
+	 * test_reverseArray test case. In order to isolate the external (to this class)
+	 * dependencies (FileIO) we have to mock them and predefine the results of all
+	 * calls that we plan to execute on the FileIO methods.
 	 */
+	FileIO fi = mock(FileIO.class);
+	IntegerOperations io = mock(IntegerOperations.class);
+	ArrayOperations arop = new ArrayOperations(fi, io);
+
 	@Test
 	public void test_reverseArray_Mocking() {
-
-		FileIO fi = mock(FileIO.class);
-		IntegerOperations io = mock(IntegerOperations.class);
-		ArrayOperations arop = new ArrayOperations(fi, io);
 
 		String filepath = "src/test/resources/FileIo_ValidTestFile.txt";
 
@@ -42,20 +41,16 @@ public class ArrayOperationsTest {
 		Assert.assertArrayEquals(testValues, arop.reverseArray(filepath));
 
 	}
-	
 
 	/*
-	 * This test case examines the exception caused when
-	 * the file is empty, thus there is anothing to be revered.
-	 * Mocking is again used predefining the FileIo use.
+	 * This test case examines the exception caused when the file is empty, thus
+	 * there is anothing to be revered. Mocking is again used predefining the FileIo
+	 * use.
 	 */
-	@Test (expected = IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void test_reverseArray_Exception() {
-		FileIO fi = mock(FileIO.class);
-		IntegerOperations io = mock(IntegerOperations.class);
 
 		String filepath = "src/test/resources/Empty.txt";
-		ArrayOperations arop = new ArrayOperations(fi, io);
 		
 		int[] fileInput = {};
 		when(fi.readFile(filepath)).thenReturn(fileInput);
@@ -63,22 +58,15 @@ public class ArrayOperationsTest {
 		arop.reverseArray(filepath);
 	}
 
-
 	/*
-	 * The following test aims to fix the problem described in 
-	 * the previous test_reverseArray test case. In order to 
-	 * isolate the external (to this class) dependencies (FileIO and 
-	 * IntegerOperations) we have to mock them and predefine 
-	 * the results of all calls that we plan to execute on the
-	 * FileIO and IntegerOperations methods.
+	 * The following test aims to fix the problem described in the previous
+	 * test_reverseArray test case. In order to isolate the external (to this class)
+	 * dependencies (FileIO and IntegerOperations) we have to mock them and
+	 * predefine the results of all calls that we plan to execute on the FileIO and
+	 * IntegerOperations methods.
 	 */
 	@Test
 	public void test_findMaxInFile_Mocking() {
-
-		FileIO fi = mock(FileIO.class);
-		IntegerOperations io = mock(IntegerOperations.class);
-
-		ArrayOperations arop = new ArrayOperations(fi, io);
 
 		String filepath = "src/test/resources/ArrayTest.txt";
 
@@ -88,26 +76,24 @@ public class ArrayOperationsTest {
 		Assert.assertEquals(4, arop.findMaxInFile(filepath));
 
 	}
-	
+
 	/*
-	 * This test case examines the exception caused when
-	 * the file is empty, thus there is anothing to be revered.
-	 * Mocking is again used predefining the FileIo and Integer-
-	 * Operations use.
+	 * This test case examines the exception caused when the file is empty, thus
+	 * there is anothing to be revered. Mocking is again used predefining the FileIo
+	 * and Integer- Operations use.
 	 */
-	@Test (expected = IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void test_findMaxInFile_Exception() {
 		FileIO fi = mock(FileIO.class);
 		IntegerOperations io = mock(IntegerOperations.class);
 
 		String filepath = "src/test/resources/Empty.txt";
 		ArrayOperations arop = new ArrayOperations(fi, io);
-		
+
 		int[] fileInput = {};
 		when(fi.readFile(filepath)).thenReturn(fileInput);
-	
+
 		arop.reverseArray(filepath);
 	}
-
 
 }
